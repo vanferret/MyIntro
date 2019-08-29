4a84ea77fec0af9c34a33d9c31fbb2642ee0b549
Van Anderson <van.anderson.2@alight.com>
2019-08-28 22:32:11 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("My name is Van Anderson")
+    println("I have a daughter named Emma")
+    println("I am excited to be learning mobile app development")
+    println("I just recently switched from Apple to Android")
+    println("I want to make apps for both iOS and Android!")
+}
