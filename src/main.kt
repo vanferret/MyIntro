76a3557a54945590aef8b2d7ea45e54b29297ee5
vanferret <vanferret@gmail.com>
2019-08-28 22:48:37 -0500

    making an update

--- src/main.kt
+++ src/main.kt
@@ -4,4 +4,5 @@ fun main(args: Array<String>) {
     println("I am excited to be learning mobile app development")
     println("I just recently switched from Apple to Android")
     println("I want to make apps for both iOS and Android!")
+    println("I want to add a line and push/commit")
 }
